cd03e8099d3ba1c97d91991fcf95fb1b4cbe6876
Sergiy Chernets <sergiy.chernets@mediavalet.com>
2016-02-22 16:59:47 -0800

    Add AssetId ValueObject type

---
+++ SampleBoundedContext/Models/Asset/AssetId.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using MediaValet.Core.DDD;
+
+namespace SampleBoundedContext.Models.Asset
+{
+    public class AssetId : ValueObject<AssetId>
+    {
+        private readonly int _hashCode;
+
+        public AssetId(Guid id)
+        {
+            Id = id;
+            _hashCode = new { Id }.GetHashCode();
+        }
+
+        public Guid Id
+        {
+            get;
+        }
+        public override int GetHashCode()
+        {
+            return _hashCode;
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType()}[{Id}]";
+        }
+
+        public override bool Equals(AssetId other)
+        {
+            return other != null && Id.Equals(other.Id);
+        }
+    }
+}
